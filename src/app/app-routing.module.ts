15044967de43e3935a376af89f9f11162d2f1241
khanhcao99 <khanhboy1324@gmail.com>
2022-09-29 15:37:44 +0700

    giao dien lan 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ShopComponent} from "./shop/shop.component";
+import {HomeComponent} from "./home/home.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component: HomeComponent,
+  },
+  {
+    path:"shop", component: ShopComponent,
+    children: [
+      {
+        path: "shopping-cart", component: ShoppingCartComponent
+      },
+      {
+        path: "", component: HomeComponent
+      }
+    ]
+  },
+  {
+    path:"shopping-cart", component: ShoppingCartComponent,
+    children: [
+      {
+        path: "shop", component: ShopComponent
+      },
+      {
+        path: "", component: HomeComponent
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [HomeComponent, ShopComponent, ShoppingCartComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { ShopComponent } from './shop/shop.component';
+import {HomeComponent} from "./home/home.component";
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    RoutingComponent,
+    ShoppingCartComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

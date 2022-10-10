62bfd3900b655116b7af51cba5f274c5300e2166
NguyenViet1306 <vietsongtu@gmail.com>
2022-10-10 09:17:08 +0700

    up truoc pull

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import {OrderService} from "../service/order.service";
 import {Orders} from "../model/Order";
 
+
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+  listOrder: Orders[] = []
 
-  orderList: Orders[] = []
+  displayedColumns: string[] = ['', 'customer', 'price', 'Action'];
+  // dataSource = ELEMENT_DATA;
+
   constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
@@ -18,11 +22,37 @@ export class OrderComponent implements OnInit {
 
   displayAllOrder(){
     this.orderService.findAllOrder().subscribe(value => {
-      this.orderList= value
+      this.listOrder= value
       console.log(value)
     })
   }
-
-
-
 }
+
+// export interface Order {
+//   customer_id:string,
+//   status_order: number,
+//   status_exist: number,
+//   status_pay: number
+// }
+
+
+// export interface Order_Detail {
+//   image: string;
+//   name: string;
+//   quantity: number;
+//   symbol: string;
+// }
+
+// const ELEMENT_DATA: Order_Detail[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
+
